3e15d5ea28e64ad7f30189105efd3e42336dfd3d
Alexandr Vedenyapin <avedenyapin@kaaiot.io>
2018-08-30 21:38:21 +0300

    Initial commit

---
+++ app/src/main/java/com/alex/vedenyapin/imageloader/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.alex.vedenyapin.imageloader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
