32b0fc00663e4d15a87b73fe8fb8142bce2e4b1c
Alexandr Vedenyapin <avedenyapin@kaaiot.io>
2018-08-31 09:52:27 +0300

    Added Retrofit.

---
+++ app/src/main/java/com/alex/vedenyapin/imageloader/network/Api.kt
@@ -0,0 +1,21 @@
+package com.alex.vedenyapin.imageloader.network
+
+import android.media.Image
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+/**
+ * Created by Alex Vedenyapin on 31.08.2018
+ */
+
+/**
+ * The interface which provides methods to get result of webservices
+ */
+interface Api {
+
+    /**
+     * Get the list of the images from the API
+     */
+    @GET("/photos")
+    fun getImages(): Observable<List<Image>>
+}
---
+++ app/src/main/java/com/alex/vedenyapin/imageloader/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.alex.vedenyapin.imageloader.utils
+
+/**
+ * Created by Alex Vedenyapin on 31.08.2018
+ */
+/** The base URL of the API */
+const val BASE_URL: String = "https://jsonplaceholder.typicode.com"
