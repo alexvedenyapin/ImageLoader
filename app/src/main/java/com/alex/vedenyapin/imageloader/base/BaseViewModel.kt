28e5798a01de3a616c81499055b0a9cda56f2f0d
Alexandr Vedenyapin <avedenyapin@kaaiot.io>
2018-08-31 09:46:37 +0300

    Added Image and BaseViewModel.

---
+++ app/src/main/java/com/alex/vedenyapin/imageloader/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.alex.vedenyapin.imageloader.base
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by Alex Vedenyapin on 31.08.2018
+ */
+class BaseViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/alex/vedenyapin/imageloader/model/Image.kt
@@ -0,0 +1,6 @@
+package com.alex.vedenyapin.imageloader.model
+
+/**
+ * Created by Alex Vedenyapin on 31.08.2018
+ */
+data class Image(val albumId: Int, val id: Int, val title: String, val url: String, val thumbnailUrl: String)
